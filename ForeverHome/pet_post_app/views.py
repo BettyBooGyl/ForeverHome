71648799d534c8b8c60ecb7bd70982bb995f4764
KingPhilip14 <kingian404@gmail.com>
2023-05-04 00:07:57 -0500

    Finalized Liked Pet Posts
    These features are done except for a few linking set ups.

--- ForeverHome/pet_post_app/views.py
+++ ForeverHome/pet_post_app/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from pet_post_app.models import Post
+
 
 def search(request):
     return render(request, 'pet_post_app/browsing_page.html')
+
+
 def create_new_post_page(request):
     return render(request, 'pet_post_app/create_new_post.html')
+
+
 def detail_pet_post(request):
     return render(request, 'pet_post_app/detail_pet_post.html')
+
+
 def liked_pet_post(request):
-    return render(request, 'pet_post_app/liked_pet_post.html')
+    post_list = Post.objects.order_by('id')
+    post_dict = {'liked_posts': post_list}
+    return render(request, 'pet_post_app/liked_pet_post.html', context=post_dict)
