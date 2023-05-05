d908d8892d6ce383983f77c8b48efb9801aa1e16
kylynnerickson <125531228+kylynnerickson@users.noreply.github.com>
2023-05-05 00:12:43 -0500

    SearchUpdate

--- ForeverHome/pet_post_app/urls.py
+++ ForeverHome/pet_post_app/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('liked_posts', views.liked_pet_post, name='liked_posts'),
     path('new_post', views.create_new_post_page, name='new_post'),
     path('details', views.detail_pet_post, name='details'),
+    
+    #searchthings -kylynn
+        path('pet/<str:slug>/<int:id>', views.pet_detail, name='pet_detail'),
+        path('filter-data', views.filter_data, name='filter_data'),
+
 
 ]
